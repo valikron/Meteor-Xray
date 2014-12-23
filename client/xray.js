50554100f442a8ec10da30488d3c24562929e457
Slava Kim <slava@meteor.com>
2014-12-22 23:49:07 -0800

    Rewrite package to Meteor 1.0

--- client/xray.js
+++ client/xray.js
@@ -1,65 +1,67 @@
 Meteor.startup(function() {
-    UI.materialize(Template.xray, document.body);
-    Session.set('xray-label', 'xray on');
+  Blaze.render(Template.xray, document.body);
+  Session.set('xray-label', 'xray on');
 });
 
-Template.xray.label = function() {
+Template.xray.helpers({
+  label: function () {
     return Session.get('xray-label');
-};
+  }
+});
 
 var xrayVisible = false;
 
 Template.xray.events({
-    'click button': function(e, tpl) {
-        var regions;
+  'click button': function(e, tpl) {
+    var regions;
 
-        if (!xrayVisible) {
-            regions = document.body.querySelectorAll('.xray');
+    if (!xrayVisible) {
+      regions = document.body.querySelectorAll('.xray');
 
-            Session.set('xray-label', 'xray off');
+      Session.set('xray-label', 'xray off');
 
-            _.each(regions, function(region) {
-                region.className = region.className.replace('xray', 'xray-visible');
-            });
+      _.each(regions, function(region) {
+        region.className = region.className.replace('xray', 'xray-visible');
+      });
 
-            xrayVisible = !xrayVisible;
-        } else {
-            regions = document.body.querySelectorAll('.xray-visible');
+      xrayVisible = !xrayVisible;
+    } else {
+      regions = document.body.querySelectorAll('.xray-visible');
 
-            Session.set('xray-label', 'xray on');
+      Session.set('xray-label', 'xray on');
 
-            _.each(regions, function(region) {
-                region.className = region.className.replace('xray-visible', 'xray');
-            });
+      _.each(regions, function(region) {
+        region.className = region.className.replace('xray-visible', 'xray');
+      });
 
-            xrayVisible = !xrayVisible;
-        }
+      xrayVisible = !xrayVisible;
     }
+  }
 });
 
 // render and put in the document
 var renderToDiv = function(comp) {
-    var div = document.createElement('DIV');
-    var innderDiv = document.createElement('DIV');
-    innderDiv.className = 'xray-label';
-    div.appendChild(innderDiv);
-    div.className = 'xray-label-container';
-    UI.materialize(comp, innderDiv);
-    return div;
+  var div = document.createElement('DIV');
+  var innderDiv = document.createElement('DIV');
+  innderDiv.className = 'xray-label';
+  div.appendChild(innderDiv);
+  div.className = 'xray-label-container';
+  Blaze.render(comp, innderDiv);
+  return div;
 };
 
 Template.rendered(null, function() {
-    var self = this;
+  var self = this;
 
-    if (self.templateName !== 'xray') {
-        // get direct childs
-        var childs = self.findAll('>*');
+  if (self.templateName !== 'xray') {
+    // get direct childs
+    var childs = self.findAll('>*');
 
-        _.each(childs, function(child) {
-            child.className += ' xray';
-            var templateLabel = renderToDiv(self.templateName);
-            child.insertBefore(templateLabel, child.firstNode);
-        });
+    _.each(childs, function(child) {
+      child.className += ' xray';
+      var templateLabel = renderToDiv(self.templateName);
+      child.insertBefore(templateLabel, child.firstNode);
+    });
 
-    }
+  }
 });
